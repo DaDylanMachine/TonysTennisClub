9666bb8b1decb04c1e97ae8ca2ebc84fed93bb19
FSzyga <franciscosyquia@gmail.com>
2024-03-03 23:08:33 -0500

    Bucket stuff eventually

---
+++ Assets/Scripts/Objective Scripts/BucketObj.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BucketObj : MonoBehaviour
+{
+    // GameObject and Component variables to edit them within the script.
+    public GameObject waterBucket;
+    public Camera playerCamera;
+    public PlayerManager playerManager;
+    public ItemSwap itemSwap;
+    public Transform equipChild;
+    // Float variable that determines the distance of the Raycast.
+    public float pickupRange = 10f;
+    // Int variable that keeps track of how many pieces of trash have been thrown away.
+    public int trashDeleted = 0;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            // Raycast is made.
+            Ray cameraRay = playerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0.5f));
+            // Check if the raycast hits a colldier.
+            if (Physics.Raycast(cameraRay, out RaycastHit hitInfo, pickupRange))
+            {
+                // Checks if the raycast is hitting the trash can GameObject and if the item they are holding is trash.
+                if (hitInfo.collider.tag == "waterCooler" && playerManager.equippedItem.CompareTag("bucket"))
+                {
+                    // If it is, destroy the trash item, denote that there is no equipped item, and track that another piece of trash has been deleted.
+                    Debug.Log("bucket test");
+                }               
+            }
+        }
+    }
+}
