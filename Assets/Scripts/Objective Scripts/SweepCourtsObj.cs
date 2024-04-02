8f5ae7b5ad6eb73af56f87695c058d5d2647c8b3
Dylan George <beanmachine2001@yahoo.com>
2024-04-02 17:34:30 -0400

    Version 1
    - Implemented the basics of brush court task

--- Assets/Scripts/Objective Scripts/SweepCourtsObj.cs
+++ Assets/Scripts/Objective Scripts/SweepCourtsObj.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class SweepCourtsObj : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // GameObject and Component variables to edit them within the script.
+    public Material newMaterial;
+    public MeshRenderer courtTexture;
+    public float timer = 0f;
+    public float timeRate = 1.3f;
+    public float finalTime = 15f;
+
+    // Function that changes the Court Texture to Brushed after the cart is on the court for about 10 seconds.
+    private void OnCollisionStay(Collision collision)
     {
-        
+        if (collision.gameObject.tag == "Cart")
+        {
+            timer += timeRate * Time.deltaTime;
+            if (timer >= finalTime)
+            {
+                Debug.Log("poggers");
+                courtTexture.material = newMaterial;
+            }
+
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    // Resets the timer for brushing courts.
+    private void OnCollisionExit(Collision collision)
     {
-        
+        timer = 0f;
     }
 }
